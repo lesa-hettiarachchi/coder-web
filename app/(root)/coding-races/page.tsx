06e41fa7759c31196e93148ed948817a410bb8ca
lesa <lesandulinal@gmail.com>
2025-08-19 17:08:00 +1000

    Initial routing layout

---
+++ app/(root)/coding-races/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>coding races</div>
+  )
+}
+
+export default page
---
+++ app/(root)/escape-room/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1>Escape Rooms</h1>
+        <span>Comming Soon....</span>
+    </div>
+  )
+}
+
+export default page
